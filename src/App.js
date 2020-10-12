e86790250fc557428dd35ede89ba83b6ad6b4dcc
Mahmoud <mymafm0405@gmail.com>
2020-10-12 19:23:01 +0300

    add router to our App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+
+const HatsPage = (props) => {
+  console.log(props);
+return (
+  <div>
+    <h1>HATS PAGE COMP.</h1>
+  </div>
+);
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
